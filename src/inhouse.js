6fbf32699af7a6d0c380a6b0f2aef16c324d2bb2
Garrett <gmoseke@gmail.com>
2019-09-11 15:36:35 -0600

    Some inhouse backend stuff
    Added inhouse file, object structure
    Methods for sorting teams and stubs for getting api data included

---
+++ src/inhouse.js
@@ -0,0 +1,159 @@
+//Represents a full inhouse
+class Inhouse 
+{
+    constructor() 
+    {
+        this.players = [];
+
+        void function addPlayerToInhouse(newPLayerDiscordName) 
+        {
+            var newPLayer = new Player(newPLayerDiscordName)
+            players.push(newPLayer);
+        };
+
+        this.currentMatch = new Match(this.players);
+    }
+
+    // UpdateMatch()
+    // {
+
+    // }
+}
+
+//Represents an individual player in the inhouse
+//Stores discord username and riot api data
+class Player 
+{
+    //takes a player's discord username, translates to linked summoner id
+    constructor(playerDiscordName) 
+    {
+        this.discordName = playerDiscordName;
+        this.summonerName = getSummonerName(playerDiscordName);
+        this.summonerID = getSummonerId(this.summonerName);
+        this.rankWeight = calculateRankWeight();
+        
+        //retrieves summoner name from discord name
+        function getSummonerName(discordName) 
+        {
+            var _summonerName;
+            //pull json data
+
+            return _summonerName;
+        }
+
+        //retrieves summoner id from summoner name
+        function getSummonerId(name)
+        {
+            var _summonerId;
+            //pull json data
+
+            return _summonerId;
+        }
+
+        //calculates the weight of the player's rank from their id
+        function calculateRankWeight(summId) 
+        {
+            var _rankWeight = 0;
+
+            //pull json data
+            var tier;
+            var division;
+
+            //calculate weight
+            switch(tier)
+            {
+                case "IRON":
+                    _rankWeight += 0;
+                    break;
+                case "BRONZE":
+                    _rankWeight += 10;
+                    break;
+                case "SILVER":
+                    _rankWeight += 20;
+                    break;
+                case "GOLD":
+                    _rankWeight += 30;
+                    break;
+                case "PLATINUM":
+                    _rankWeight += 40;
+                    break;
+                case "DIAMOND":
+                    _rankWeight += 50;
+                    break;
+                case "MASTER":
+                    _rankWeight += 80;
+                    break;
+                case "GRANDMASTER":
+                    _rankWeight += 90;
+                    break;
+                case "CHALLENGER":
+                    _rankWeight += 100;
+                    break;
+                default:
+                    console.log("tier: " + tier);
+            }
+            switch(division)
+            {
+                case "I":
+                    _rankWeight += 8;
+                    break;
+                case "II":
+                    _rankWeight += 6;
+                    break;
+                case "III":
+                    _rankWeight += 4;
+                    break;
+                case "IV":
+                    _rankWeight += 2;
+                    break;
+                default:
+                    _rankWeight += 0;
+                    break;
+            }
+
+            return _rankWeight;
+        }
+    }
+}
+
+//The data representation of one inhouse match, with sorted teams
+class Match
+{
+    //takes a list of player objects
+    constructor(players)
+    {
+        var playerDict = {};
+
+        if(players.length != 10)
+        {
+            //TODO: better error handle
+            console.error("need 10 players");
+            return;
+        }
+
+        this.team1 = [];
+        this.team2 = [];
+
+        void function sortTeams()
+        {
+            //sort in descending order by rank weight
+            players.sort(function(a, b) {
+                return b.rankWeight - a.rankWeight;
+            });
+
+            for(var i = 0; i < players.length; i++)
+            {
+                //put every other player into the corresponding team
+                if(i % 2 == 0)
+                {
+                    team1.push(players[i].summonerName);
+                }
+                else
+                {
+                    team2.push(players[i].summonerName);
+                }
+            }
+        };
+    }
+
+}
