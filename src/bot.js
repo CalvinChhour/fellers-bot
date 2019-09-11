794e695ad3b9872c5de1c6bdd36626548f69cb42
Garrett <gmoseke@gmail.com>
2019-09-11 17:31:56 -0600

    Inhouse command structure implementation
    lots of questions to ask

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ const config = require('./config.json');
 const utils = require('./utils');
 const logger = require('./logger');
 const db = require('./mongo');
+const inhouse = require('./inhouse');
 
 let initializeBot = () => {    
 	const client = new Discord.Client({
@@ -30,6 +31,7 @@ let initializeBot = () => {
 			const splitMessage = content.split(' ');
 			const command = splitMessage[1];
 			let data = splitMessage[2];
+			let modifier = splitMessage[3];
 			
 			if (command === 'help') {
 				message.channel.send('', 
@@ -86,6 +88,42 @@ let initializeBot = () => {
 			if (command === 'tft' && data) {
 				logger.info('tft command...');
 				message.channel.send(('https://tracker.gg/tft/profile/riot/NA/'+data+'/overview'));
+			}
+
+			if (command === 'inhouse' && data ) {
+				//TODO: check that an inhouse is not currently trying to be made, or allow multiple
+				//ask calvin about how exactly this works, need to make sure it persists. will not work currently
+				let currentInhouse = null;
+				switch(data) {
+					case 'start':
+						//TODO: add modifiers once implemented
+						message.channel.send('@here <user> wants to start an inhouse! Type \'!feller inhouse join <*Your League Username*> to join!\'');
+						//make a new inhouse object
+						currentInhouse = new Inhouse();
+						break;
+					case 'join':
+						if(currentInhouse === null) {
+							message.channel.send('Please start an inhouse first!');
+						} else if (modifier) {
+							let joinReturnCode = currentInhouse.addPlayerToInhouse(message.author.username, modifier);
+							if(joinReturnCode === -1) {
+								message.channel.send('That username was not recognized, please double check your spelling or any special characters and try again');
+							} 
+							if(joinReturnCode === 0) {
+								message.channel.send('Inhouse is full! Creating teams...');
+								if(currentInhouse.currentMatch !== null) {
+									message.channel.send('',
+										new Discord.RichEmbed({title: 'Lineups:'})
+											.addField('Team 1:', currentInhouse.currentMatch.team1.join('\n'), true)
+											.addField('Team 2:', currentInhouse.currentMatch.team2.join('\n'), true)
+									);
+								}
+							}
+						} else {
+							message.channel.send('Please include your League of Legends summoner name like so: !f inhouse join <summoner name>');
+						}
+						break;
+				}
 			}
 		}
 	});
