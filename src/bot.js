c6a62440c605f2c02f41fb92c073e6fea1bbe85a
Garrett <rali.grifinfyst@gmail.com>
2019-09-17 20:09:55 -0600

    Finished baseline inhouse command
    Players may create and join an inhouse
    Teams are filled and sorted once 10 players join
    sorting is either random or based on rank depending on the type of inhouse made (4fun or tryhard)

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ const config = require('./config.json');
 const utils = require('./utils');
 const logger = require('./logger');
 const db = require('./mongo');
-const inhouse = require('./inhouse');
+const Inhouse = require('./inhouse');
 
 let initializeBot = () => {    
 	const client = new Discord.Client({
@@ -23,6 +23,7 @@ let initializeBot = () => {
 	});
 
 	var currentInhouse;
+	var inhouseMessageID;
 
 	// Create an event listener for messages
 	client.on('message', async (message) => {
@@ -32,7 +33,21 @@ let initializeBot = () => {
 			const splitMessage = content.split(' ');
 			const command = splitMessage[1];
 			let data = splitMessage[2];
-			let modifier = splitMessage[3];
+
+			//TODO: make me less gross
+			let modifier = '';
+			if(splitMessage.length > 3)
+			{
+				let i = 3;
+				for(i; i< splitMessage.length; i++)
+				{
+					if(i !== splitMessage.length -1) {
+					modifier += splitMessage[i] + ' ';
+					} else {
+						modifier += splitMessage[i];
+					}
+				}
+			}
 			
 			if (command === 'help') {
 				message.channel.send('', 
@@ -93,36 +108,77 @@ let initializeBot = () => {
 
 			if (command === 'inhouse' && data ) {
 				switch(data) {
+					case 'clear':
+						currentInhouse = null;
+						break;
 					case 'start':
-						//TODO: add modifiers once implemented
 						if(currentInhouse == null) {
-							message.channel.send(message.author.username + ' wants to start an inhouse! Type \"!feller inhouse join <*Your League Username*>\" to join!');
-							currentInhouse = new Inhouse();
+							let inhouseType = '';
+							if(modifier === '4fun') {
+								inhouseType = '4fun';
+							} else {
+								inhouseType = 'tryhard';
+							}
+							message.channel.send(message.author.username + ' wants to start a ' + inhouseType + ' inhouse! Type \"!f inhouse join <*Your League Username*>\" to join!');
+							currentInhouse = new Inhouse(modifier);
+							logger.info('New ' + inhouseType + ' Inhouse created');
 						} else {
-							message.channel.send('Inhouse already exists');
+							message.channel.send('An Inhouse already exists, please use \"!f inhouse clear\" if you wish to create a new one');
 						}
 						break;
 					case 'join':
 						if(currentInhouse == null) {
 							message.channel.send('Please start an inhouse first!');
 						} else if (modifier) {
-							let joinReturnCode = currentInhouse.addPlayerToInhouse(message.author.username, modifier);
-							if(joinReturnCode === -1) {
-								message.channel.send('That username was not recognized, please double check your spelling or any special characters and try again');
-							} 
-							if(joinReturnCode === 0) {
-								message.channel.send('Inhouse is full! Creating teams...');
-								if(currentInhouse.currentMatch !== null) {
-									message.channel.send('',
-										new Discord.RichEmbed({title: 'Lineups:'})
-											.addField('Team 1:', currentInhouse.currentMatch.team1.join('\n'), true)
-											.addField('Team 2:', currentInhouse.currentMatch.team2.join('\n'), true)
-									);
+							currentInhouse.addPlayerToInhouse(message.author.username, modifier).then(respone => {
+								let joinReturnCode = respone;
+								if(joinReturnCode === -1) {
+									message.channel.send('That username was not recognized, please double check your spelling or any special characters and try again');
+								} 
+								if(joinReturnCode === 0) {
+									message.delete();
+									message.channel.send('Inhouse is full! Creating teams...');
+									team1Names = [];
+									team2Names = [];
+									currentInhouse.currentMatch.team1.forEach(element => {
+										team1Names.push(element.summonerName);
+									});
+									currentInhouse.currentMatch.team2.forEach(element => {
+										team2Names.push(element.summonerName);
+									});
+									if(currentInhouse.currentMatch !== null) {
+										let desc = ((currentInhouse.type === '4fun')?'Sorted randomly':'Sorted by Soloqueue Rank');
+										message.channel.send('',
+											new Discord.RichEmbed({title: 'Lineups: ', description: desc})
+											.addField('Team 1:', team1Names.join('\n'), true)
+											.addField('Team 2:', team2Names.join('\n')), true);
+									}
+									logger.info('Inhouse full, created teams');
 								}
-							}
-							if(joinReturnCode === 1) {
-								message.channel.send(message.author.username + ' joined!');
-							}
+								if(joinReturnCode === 1) {	
+									let joinedNames = [];
+									currentInhouse.players.forEach(element => {
+										joinedNames.push(element.summonerName);
+									});
+
+									if(inhouseMessageID == null) {
+										message.delete();
+										message.channel.send('',
+												new Discord.RichEmbed({title: 'Current Players:'})
+													.addField(joinedNames.join(', '), false)
+										).then(respone => {
+											inhouseMessageID = respone.id;
+										});
+									} else {
+										message.delete();
+										message.channel.fetchMessage(inhouseMessageID)
+										.then(message => message.edit('',
+										new Discord.RichEmbed({title: 'Current Players:'})
+											.addField(joinedNames.join(', '), false)));
+									}
+									logger.info('Player joined an inhouse');
+								}
+							});
 						} else {
 							message.channel.send('Please include your League of Legends summoner name like so: !f inhouse join <summoner name>');
 						}
--- src/inhouse.js
+++ src/inhouse.js
@@ -1,39 +1,44 @@
+const https = require('https');
+const config = require('./config.json');
+const riotKey = config.riotKey;
+const urlSummId = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+const urlSummRank = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/";
+const urlFinish = '?api_key='+ riotKey;
 
 //Represents a full inhouse
-class Inhouse 
-{
-    constructor() 
-    {
+module.exports = class Inhouse {
+    constructor(_type) {
         this.players = [];
         this.currentMatch = null;
+        this.type = _type;
     }
 
-    addPlayerToInhouse(newPLayerDiscordName, newPLayerSummonerName) 
-    {
-        var newPLayer = new Player(newPLayerDiscordName, newPLayerSummonerName)
-        //if the summoner name could not be found, return -1 to let the bot know
-        if(newPLayer.summonerID === -1)
-        {
-            return -1;
-        }
-        
-        //otherwise stick the new player into our list
-        players.push(newPLayer);
-
-        //if we've got 10 players, great, start the match
-        if(this.players.length === 10 && this.currentMatch === null)
-        {
-            this.CreateMatch();
-            //return 0 to let our bot know to stop accepting joins
-            return 0;
-        }
-
-        return 1;
+    addPlayerToInhouse(newPLayerDiscordName, newPlayerSummonerName) {
+        return new Promise((resolve, reject) =>{
+            new Player(newPLayerDiscordName, newPlayerSummonerName).then(response => {
+                var newPlayer = response;
+                 //if the summoner name could not be found, return -1 to let the bot know
+                if(newPlayer.summonerID === -1) {
+                    resolve(-1);
+                }
+                
+                //otherwise stick the new player into our list
+                this.players.push(newPlayer);
+    
+                //if we've got 10 players, great, start the match
+                if(this.players.length === 10 && this.currentMatch === null) {
+                    this.CreateMatch();
+                    //return 0 to let our bot know to stop accepting joins
+                    resolve(0);
+                }
+    
+                resolve(1);
+            });
+        });   
     }
 
-    CreateMatch()
-    {
-        this.currentMatch = new Match(this.players);
+    CreateMatch() {
+        this.currentMatch = new Match(this.players, this.type);
     }
     // UpdateMatch()
     // {
@@ -43,142 +48,203 @@ class Inhouse
 
 //Represents an individual player in the inhouse
 //Stores discord username and riot api data
-class Player 
-{
+class Player {
     //takes a player's discord username, translates to linked summoner id
-    constructor(playerDiscordName, playerSummonerName) 
-    {
-        this.discordName = playerDiscordName;
-        this.summonerName = playerSummonerName
-        this.summonerID = getSummonerId(this.summonerName);
-        //only try to pull more data if we succesfully pulled the ID
-        if(this.summonerID !== -1)
-        {
-            this.rankWeight = calculateRankWeight(this.summonerID);
-        }
+    constructor(playerDiscordName, playerSummonerName) {
+        return new Promise((resolve, reject) => {
+            this.discordName = playerDiscordName;
+            this.summonerName = playerSummonerName;
+            this.summonerID;
+            this.rankWeight;
+    
+            this.getSummonerId(this.summonerName).then(response => {
+                this.summonerID = response;
+                //only try to pull more data if we succesfully pulled the ID
+                if(this.summonerID !== null) {
+                    this.getRankWeight(this.summonerID).then(response => {
+                        this.rankWeight = response;
+                        resolve(this);
+                    });
+                }
+            });
+        });
 
-        //retrieves summoner id from summoner name
-        function getSummonerId(name)
-        {
+    }
+
+    //retrieves summoner id from summoner name
+    getSummonerId(name) {
+        return new Promise((resolve, reject) => {
             var _summonerId;
-            //track response code from server, if nonzero, error was thrown
-            var responseCode = 0;
-            //PULL/PARSE JSON DATA HERE
+            let fullUrl = urlSummId + name + urlFinish;
+        
+            //pull data
+            https.get(fullUrl, (resp) => {
+                let data = '';
 
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                    });
+                // The whole response has been received
+                resp.on('end', () => {
+                    try {
+                        var jsonResp = JSON.parse(data);
+                        _summonerId = jsonResp.id;
+                        resolve(_summonerId);
+                    } catch(e) {
+                        reject(e.message);
+                    }
 
-            if(responseCode !== 0)
-            {
-                //can send specific errors here if need be
-                return -1;
-            }
-            else
-            {
-                return _summonerId;
-            }
-        }
-
-        //calculates the weight of the player's rank from their id
-        function calculateRankWeight(summId) 
-        {
+                    });
+            }).on("error", (err) => {
+                reject(`Got error: ${err.message}`);
+            });
+        });
+    }
+    
+    //returns a summoner's solo queue rank weight
+    getRankWeight(summId) {
+        return new Promise((resolve, reject) => {
             var _rankWeight = 0;
-
-            //pull json data
             var tier;
             var division;
+    
+            let fullUrl = urlSummRank + summId + urlFinish;
+            //pull json data
+            https.get(fullUrl, (resp) => {
+                let data = '';
+    
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                  });
+                // The whole response has been received
+                resp.on('end', () => {
+                    try {
+                        var jsonResp = JSON.parse(data)
+                        var i;
+                        for(i = 0; i < 3; i++) {
+                            if(jsonResp[i] != null) {
+                                if(jsonResp[i].queueType === 'RANKED_SOLO_5x5') {
+                                    tier = jsonResp[i].tier;
+                                    division = jsonResp[i].rank;
+                                    break;
+                                }
+                            }
+                        }
 
-            //calculate weight
-            switch(tier)
-            {
-                case "IRON":
-                    _rankWeight += 0;
-                    break;
-                case "BRONZE":
-                    _rankWeight += 10;
-                    break;
-                case "SILVER":
-                    _rankWeight += 20;
-                    break;
-                case "GOLD":
-                    _rankWeight += 30;
-                    break;
-                case "PLATINUM":
-                    _rankWeight += 40;
-                    break;
-                case "DIAMOND":
-                    _rankWeight += 50;
-                    break;
-                case "MASTER":
-                    _rankWeight += 80;
-                    break;
-                case "GRANDMASTER":
-                    _rankWeight += 90;
-                    break;
-                case "CHALLENGER":
-                    _rankWeight += 100;
-                    break;
-                default:
-                    console.log("tier: " + tier);
-            }
-            switch(division)
-            {
-                case "I":
-                    _rankWeight += 8;
-                    break;
-                case "II":
-                    _rankWeight += 6;
-                    break;
-                case "III":
-                    _rankWeight += 4;
-                    break;
-                case "IV":
-                    _rankWeight += 2;
-                    break;
-                default:
-                    _rankWeight += 0;
-                    break;
-            }
-
-            return _rankWeight;
-        }
+                        //calculate weight
+                        switch(tier) {
+                            case "IRON":
+                                _rankWeight += 0;
+                                break;
+                            case "BRONZE":
+                                _rankWeight += 10;
+                                break;
+                            case "SILVER":
+                                _rankWeight += 20;
+                                break;
+                            case "GOLD":
+                                _rankWeight += 30;
+                                break;
+                            case "PLATINUM":
+                                _rankWeight += 40;
+                                break;
+                            case "DIAMOND":
+                                _rankWeight += 50;
+                                break;
+                            case "MASTER":
+                                _rankWeight += 80;
+                                break;
+                            case "GRANDMASTER":
+                                _rankWeight += 90;
+                                break;
+                            case "CHALLENGER":
+                                _rankWeight += 100;
+                                break;
+                            default:
+                                _rankWeight += 20;
+                        }
+                        switch(division) {
+                            case "I":
+                                _rankWeight += 8;
+                                break;
+                            case "II":
+                                _rankWeight += 6;
+                                break;
+                            case "III":
+                                _rankWeight += 4;
+                                break;
+                            case "IV":
+                                _rankWeight += 2;
+                                break;
+                            default:
+                                _rankWeight += 0;
+                                break;
+                        }
+                        resolve(_rankWeight);
+                    } catch(e) {
+                        reject(e.message);
+                    }
+                  });
+            }).on("error", (err) => {
+                reject(`Got error: ${err.message}`);
+            });
+        });
+       
     }
 }
 
 //The data representation of one inhouse match, with sorted teams
-class Match
-{
+class Match {
     //takes a list of player objects
-    constructor(players)
-    {
-        if(players.length != 10)
-        {
-            //TODO: better error handle
+    constructor(players, _type) {
+        if(players.length != 10) {
             console.error("need 10 players");
             return;
         }
 
         this.team1 = [];
         this.team2 = [];
+        this.type = _type;
 
-        void function sortTeams()
-        {
+        this.sortTeams(players);
+    }
+
+    sortTeams(players) {
+        ///TODO: Maybe can make this cleaner?
+        if(this.type === '4fun') {
+            players.forEach(element => {
+                let teamAssignment = Math.random(1, 3);
+                if(teamAssignment === 1) {
+                    if(this.team1.length < 5) {
+                        this.team1.push(element);
+                    } else {
+                        this.team2.push(element);
+                    }
+                } else {
+                    if(this.team2.length < 5) {
+                        this.team2.push(element);
+                    } else {
+                        this.team1.push(element);
+                    }
+                }
+            });
+        } else {
             //sort in descending order by rank weight
             players.sort(function(a, b) {
                 return b.rankWeight - a.rankWeight;
             });
 
-            for(var i = 0; i < players.length; i++)
-            {
+            for(var i = 0; i < players.length; i++) {
                 //put every other player into the corresponding team
-                if(i % 2 == 0)
-                {
-                    team1.push(players[i]);
+                if(i % 2 == 0) {
+                    this.team1.push(players[i]);
                 }
-                else
-                {
-                    team2.push(players[i]);
+                else {
+                    this.team2.push(players[i]);
                 }
             }
-        };
+        }
     }
-
 }
