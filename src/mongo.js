716d38802bcdf75327b22df696ffd48ab066d13e
Calvin Chhour <calvin.chhour@gmail.com>
2019-09-21 02:21:31 -0700

    Changed sending of discord messages to async/await

--- src/mongo.js
+++ src/mongo.js
@@ -3,8 +3,12 @@ const logger = require('./logger');
 const MongoClient = require('mongodb').MongoClient;
 
 module.exports = (async () => {
-	const client = await MongoClient.connect(config.db.uri, config.db.options);
-	const db = await client.db(config.db.table);
-	logger.info(`Database: ${config.db.table} loaded`);
-	return db;
+	try {
+		const client = await MongoClient.connect(config.db.uri, config.db.options);
+		const db = await client.db(config.db.table);
+		logger.info(`Database: ${config.db.table} loaded`);
+		return db;
+	} catch (error) {
+		logger.error(error);
+	}
 })();
