56c632788b8375a3122d08756a14e4d53d2e3483
Calvin Chhour <calvin.chhour@gmail.com>
2019-09-24 22:15:07 -0700

    Fixed up help, added pic of kidwithnoshoes (#17)

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,14 +6,25 @@ module.exports = {
 		name : 'help',
 		args : false,
 		description : 'Command to call for help from the Discord bot',
-		execute: (message) => {
-			message.channel.send('',
-				new Discord.RichEmbed({description: '!f :KannaWave: - creates big emote \
-                                                                          \n !f avatar - sends big avatar of user\
-                                                                          \n !f whoami - sends username\
-                                                                          \n !f tft Jacob_Hong - sends tft profile ' }))
-				.then(() => logger.info('Help message relayed'))
-				.catch(err => logger.error(`Help failed: ${err}`));
+		execute: async (message) => {
+			try {
+				const response = await message.channel.send('',
+					new Discord.RichEmbed()
+						.setTitle('I\'m the fellers bot - developed by your local\
+								\nfellers. Here are some commands you can use.  ')
+						.setDescription('`!f emote :KannaWave:` - creates big emote\
+										\n`!f avatar` - sends big avatar of user\
+										\n`!f whoami` - sends username\
+										\n`!f tft Jacob_Hong` - sends tft profile')
+						.setThumbnail('https://i.imgur.com/2vtQUck.png')
+						.setColor(message.member.displayHexColor)
+				);
+				if (response) {
+					logger.info('Help message relayed');
+				}
+			} catch (error) {
+				logger.error(`Help failed: ${error}`);
+			}
 		}
 	},
 };
