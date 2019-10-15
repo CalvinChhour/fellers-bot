b35f23bd3cbbb34d8fbbbfff5cc12f905ed9be56
z6pei <z6pei@uwaterloo.ca>
2019-10-15 02:10:54 -0400

    support add movie, list and clear the movie list

---
+++ src/commands/movienight.js
@@ -0,0 +1,99 @@
+const Discord = require('discord.js');
+const logger = require('./../logger');
+
+module.exports = {
+	store : {
+		name : 'movienight',
+		args : true,
+		description : 'add/vote/select the movie for movie nights',
+		execute: async (message, data, mongo) => {
+
+			logger.info('movie night command...');
+			const movieCommand = data[0];
+			logger.info(movieCommand);
+			const movie = data.slice(1).join(' ');
+			logger.info('ADDING MOVIE' + movie);
+			try {
+				const movieEntry = await mongo.collection('Movie').findOne({_id: 'movienight'});
+				switch (movieCommand) {
+					case undefined:
+						logger.info('undefined command');
+						const response = await message.channel.send('',
+						new Discord.RichEmbed()
+							.setTitle('Use following commands to choose your movie!')
+							.setDescription('`!f movienight list`              - Display current watchlist\
+											\n`!f movienight add <movie1>`      - add a new movie,\
+											\n `!f movienight vote <movie>`    - vote for a movie; NOT WORKIN YET \
+											\n `!f movienight unvote <movie>`  - unvote for a movie; NOT WORKIN YET \
+											\n`!f movienight currentpick`     - sends the movie with the most vote; NOT WORKIN YET \
+											\n`!f movienight clear`            - clear everything for the current watchlist USE WITH CAUTIONN'));
+						break;
+					case 'list':
+						if (!movieEntry) {
+							message.channel.send('',
+							new Discord.RichEmbed({description: 'There is currently no watchlist'}));
+						}else{
+							movieList = movieEntry.movieList
+							logger.info(movieList);
+							var exampleEmbed = new Discord.RichEmbed({title: 'Watch List', 
+																	  description: 'Here are some feller proposed movie'}).setColor(0xfedc56)
+							for (var key in movieList) {
+								console.log( movieList[key])
+								exampleEmbed = exampleEmbed.addField(key, movieList[key].join('\n'), true)
+							}
+
+							message.channel.send('', exampleEmbed);
+						}
+						break;
+					case 'add':
+						logger.info(message.author.username);
+						if (movie == "") {
+							message.channel.send('',
+								new Discord.RichEmbed({description: 'please specify the movie you wanna add'}));
+						}else if(movieEntry) {
+							var newMovieQuery = {}
+							newMovieQuery["movieList."+movie] = [message.author.username]
+
+							var movieNameQuery = {}
+							movieNameQuery["movieList."+movie] = { $exists: true }
+
+							var proposerNameQuery = {}
+							proposerNameQuery["movieList."+movie] = [message.author.username]
+							const existingMovie = await mongo.collection('Movie').findOne(movieNameQuery);
+
+							if (!existingMovie) {
+								mongo.collection('Movie').updateOne({_id: 'movienight'},
+                     										    {"$set": newMovieQuery});
+								message.channel.send('',
+									new Discord.RichEmbed({description: movie+' is added successfully'}));
+								logger.info('movie info updated');
+							}else{
+								mongo.collection('Movie').updateOne({_id: 'movienight'},
+                     										        {"$addToSet": proposerNameQuery});
+								logger.info('movie already existed');
+							}
+						}else{
+							var newEntry = {}
+							newEntry[movie] = []
+							await mongo.collection('Movie').insertOne({_id: 'movienight', movieList: newEntry});
+							message.channel.send('',
+									new Discord.RichEmbed({description: movie+' is added successfully'}));
+							logger.info('movie info created and inserted');
+						}
+						break;
+					case 'clear':
+						if (!movieEntry) {
+							message.channel.send('',
+							new Discord.RichEmbed({description: 'There is currently no watchlist to be cleared'}));
+						}else{
+							await mongo.collection('Movie').deleteOne({_id: 'movienight'});
+							new Discord.RichEmbed({description: 'Movie list has been clear'});
+						}
+						break;
+				}	
+			} catch (err) {
+				logger.error(err); 
+			}
+		}
+	},
+};
